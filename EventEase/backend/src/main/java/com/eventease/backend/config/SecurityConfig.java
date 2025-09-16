efca040b873e0c2eb7c3cf70c05e5270cea62db6
Yousif Sabti <yousif.sabti@uha.fr>
2025-09-16 11:04:11 +0200

    api

--- EventEase/backend/src/main/java/com/eventease/backend/config/SecurityConfig.java
+++ EventEase/backend/src/main/java/com/eventease/backend/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.eventease.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,13 +9,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .formLogin(form -> form.disable())
+            .httpBasic(httpBasic -> httpBasic.disable());
         return http.build();
     }
 }
